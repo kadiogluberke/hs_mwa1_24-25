e468892b20e0e27fcf57b012fc9060c994f7224c
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-20 18:47:59 +0100

    REFACTOR 🔨 change styles cdn to css

--- resources/views/layouts/site/layout.blade.php
+++ resources/views/layouts/site/layout.blade.php
@@ -11,7 +11,12 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    @endif
 
 </head>
 <body class="font-sans antialiased h-full bg-yellow-100">
