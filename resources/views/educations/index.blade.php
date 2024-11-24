3cd364af306b0f4b0cf4da10687c72052b402ef7
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-24 23:27:43 +0100

    IMPROVE ⭐  improve index view, redirecting show page

--- resources/views/educations/index.blade.php
+++ resources/views/educations/index.blade.php
@@ -14,33 +14,35 @@
     <a href="{{ route('educations.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded mb-4 inline-block">Add New Education</a>
         
     @foreach($educations as $education)
-        
-        <!-- Institution Name -->
-        <div class="text-lg font-bold">
-            {{ $education->institution_name }}
-            <span class="line-end text-gray-500 text-sm">
-                {{ $education->started_at }} - {{ $education->finished_at ?? 'Present' }}
-            </span>
-        </div>
+        <a href="{{ route('educations.show', $education->id) }}">
+            
+            <!-- Institution Name -->
+            <div class="text-lg font-bold mt-1">
+                {{ $education->institution_name }}
+                <span class="line-end text-gray-500 text-sm">
+                    {{ $education->started_at }} - {{ $education->finished_at ?? 'Present' }}
+                </span>
+            </div>
 
-        <!-- Programme and Grade -->
-        <div class="text-sm text-gray-700 mt-1">
-            {{ $education->programme }}
-            <span class="line-end text-gray-500">
-                <strong>Grade:</strong> {{ $education->grade }}
-            </span>
-        </div>
+            <!-- Programme and Grade -->
+            <div class="text-sm text-gray-700 mt-2">
+                {{ $education->programme }}
+                <span class="line-end text-gray-500">
+                    <strong>Grade:</strong> {{ $education->grade }}
+                </span>
+            </div>
 
-        <!-- Description -->
-        <div class="text-sm text-gray-600 mt-2">
-            {{ $education->description }}
-        </div>
+            <!-- Description -->
+            <div class="text-sm text-gray-600 mt-3">
+                {{ Str::limit($education->description, 220, '...') }}
+            </div>
 
-        <div>
-            @foreach($education->skills as $skill)
-                <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs">{{$skill->name}}</span>
-            @endforeach
-        </div>
+            <div class="mt-4">
+                @foreach($education->skills as $skill)
+                    <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs">{{$skill->name}}</span>
+                @endforeach
+            </div>
+        </a>
 
     @endforeach
 </x-site-layout>
