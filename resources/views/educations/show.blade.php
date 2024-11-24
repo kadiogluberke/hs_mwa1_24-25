72d72dff51a3fd5f8f676aea0de24580e725474a
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-24 23:08:59 +0100

    DESIGN 🎨  create education show view

--- resources/views/educations/show.blade.php
+++ resources/views/educations/show.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+<x-site-layout title='Education Details'>
+
+    <body class="bg-gray-100 text-gray-800 font-sans">
+        
+        <!-- <h1 class="text-4xl font-bold mb-6">Education Details</h1> -->
+        <!-- Institution Name -->
+        <div class="text-2xl font-bold">
+            {{ $education->institution_name }}
+            <span class="line-end text-gray-500 text-lg">
+                {{ $education->started_at }} - {{ $education->finished_at ?? 'Present' }}
+            </span>
+        </div>
+
+        <!-- Programme and Grade -->
+        <div class="text-lg text-gray-700 mt-1">
+            {{ $education->programme }}
+            <span class="line-end text-gray-500" text-lg>
+                <strong>Grade:</strong> {{ $education->grade }}
+            </span>
+        </div>
+
+        <!-- Description -->
+        <div class="text-lg text-gray-600 mt-2">
+            {{ $education->description }}
+        </div>
+
+        <div class="mt-4">
+            @foreach($education->skills as $skill)
+                <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-base">{{$skill->name}}</span>
+            @endforeach
+        </div>
+
+        <a href="{{ route('educations.index') }}" class="bg-blue-500 text-white px-4 py-2 rounded mt-4 inline-block">Back to List</a>
+       
+    </body>
+
+</x-site-layout>
