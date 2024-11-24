bc674854e1e7fb130007d89bc2c06d9f69f50e6b
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-24 22:21:17 +0100

    DESIGN 🎨  change degree to programme

--- resources/views/educations/create.blade.php
+++ resources/views/educations/create.blade.php
@@ -33,8 +33,8 @@
                     <label for="degree" class="block text-gray-700 text-sm font-bold mb-2">Program:</label>
                     <input 
                         type="text" 
-                        id="degree" 
-                        name="degree" 
+                        id="programme" 
+                        name="programme" 
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-yellow-500"
                     >
                 </div>
