fa24af9849e86d50ca97932b8c78c2f673f50ae9
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-28 13:52:42 +0100

    FIX 🐛  solve the flashing old date in edit form

--- app/View/Components/FormText.php
+++ app/View/Components/FormText.php
@@ -5,21 +5,35 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Carbon;
 
 class FormText extends Component
 {
     /**
      * Create a new component instance.
      */
-    public function __construct(
-        public string $name,
-        public string $label,
-        public string $type = 'text',
-        public ?string $value = null,
-        public ?string $placeholder = null,
-    )
+    public $type;
+    public $name;
+    public $value;
+    public string $label;
+    public ?string $placeholder = null;
+
+    public function __construct($type = 'text', $name = null, $value = null, $label = null, $placeholder = null)   
     {
         $this->type = $type;
+        $this->name = $name;
+        $this->label = $label;
+        $this->placeholder = $placeholder;
+        $this->value = $this->formatValue($type, $value);
+    }
+
+    protected function formatValue($type, $value)
+    {
+        if ($type === 'date' && $value) {
+            return Carbon::parse($value)->format('Y-m-d');
+        }
+
+        return $value;
     }
 
     /**
--- resources/views/components/form-text.blade.php
+++ resources/views/components/form-text.blade.php
@@ -1,24 +1,13 @@
 <div class="mb-4">
     <label for="{{ $name }}" class="block text-gray-700 text-sm font-bold mb-2">{{ $label }}:</label>
     <input 
-        type="{{ $type }}"
+        type="{{ $type }}" 
         id="{{ $name }}" 
         name="{{ $name }}" 
-        value="{{ old($name , $value) }}" 
+        value="{{ old($name, $value) }}" 
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
         >
     @error($name)
         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
     @enderror
 </div>
-
-{{-- <div class="mb-4">
-                    <label for="start_date" class="block text-gray-700 text-sm font-bold mb-2">Start Date:</label>
-                    <input 
-                        type="date" 
-                        id="start_date" 
-                        name="start_date" 
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-yellow-500"
-                    >
-                </div> --}}
-
