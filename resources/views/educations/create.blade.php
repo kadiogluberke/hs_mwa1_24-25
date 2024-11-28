4cb6318b02e60d58e668f6f34a8c7e91cf47dc80
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-28 18:52:16 +0100

    REFACTOR 🔨 shift-tab

--- resources/views/educations/create.blade.php
+++ resources/views/educations/create.blade.php
@@ -2,41 +2,41 @@
 
     <x-action-layout-create title="Add New Education" :action="route('educations.store')">
 
-                <!-- Institution Field -->
-                <x-form-text name="institution_name" label="Institution" />
-    
-                <!-- Program Field -->
-                <x-form-text name="programme" label="Program" />
-    
-                <!-- Start Date Field -->
-                <x-form-text name="started_at" label="Start Date" type="date" />
-    
-                <!-- Finish Date Field -->
-                <x-form-text name="finished_at" label="Finish Date" type="date"/>
-    
-                <!-- Location Field -->
-                <x-form-text name="location" label="Location" />
-    
-                <!-- Description Field -->
-                <x-form-text-area 
-                    id="description" 
-                    name="description" 
-                    label="Description" 
-                    rows="4" 
-                />
-    
-                <!-- Grade Field -->
-                <x-form-text name="grade" label="Grade" />
+        <!-- Institution Field -->
+        <x-form-text name="institution_name" label="Institution" />
 
-                <!-- Skills Select Box -->
-                <x-skill-box 
-                    :skills="$skills" 
-                    :selectedSkills="old('skills', [])"
-                    label="Skills" 
-                    name="skills[]" 
-                    id="skills"
-                />
-        
+        <!-- Program Field -->
+        <x-form-text name="programme" label="Program" />
+
+        <!-- Start Date Field -->
+        <x-form-text name="started_at" label="Start Date" type="date" />
+
+        <!-- Finish Date Field -->
+        <x-form-text name="finished_at" label="Finish Date" type="date"/>
+
+        <!-- Location Field -->
+        <x-form-text name="location" label="Location" />
+
+        <!-- Description Field -->
+        <x-form-text-area 
+            id="description" 
+            name="description" 
+            label="Description" 
+            rows="4" 
+        />
+
+        <!-- Grade Field -->
+        <x-form-text name="grade" label="Grade" />
+
+        <!-- Skills Select Box -->
+        <x-skill-box 
+            :skills="$skills" 
+            :selectedSkills="old('skills', [])"
+            label="Skills" 
+            name="skills[]" 
+            id="skills"
+        />
+
     </x-action-layout-create>
 
 </x-site-layout>
