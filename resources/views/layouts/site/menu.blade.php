f257235231a678cabd02048a724c1d4e81d0d9e9
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-29 03:36:51 +0100

    FIX 🐛  make all menu items same class

--- resources/views/layouts/site/menu.blade.php
+++ resources/views/layouts/site/menu.blade.php
@@ -10,7 +10,7 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="{{ route('welcome') }}" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About Me</a>
+                        <a href="{{ route('welcome') }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About Me</a>
                         <a href="{{ route('educations.index') }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Educations</a>
                         <a href="{{ route('works.index') }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Works</a>
                         {{-- <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
