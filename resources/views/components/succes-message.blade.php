92d78d006041bb4730809fd4091fbab5dc5d24c7
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-29 03:05:54 +0100

    IMPROVE ⭐  add mt to succes message

--- resources/views/components/succes-message.blade.php
+++ resources/views/components/succes-message.blade.php
@@ -1,5 +1,5 @@
 @if (session('success'))
-    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
+    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4 mt-2">
         <strong class="font-bold">Success!</strong>
         <span class="block sm:inline">{{ session('success') }}</span>
         <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.remove();">
