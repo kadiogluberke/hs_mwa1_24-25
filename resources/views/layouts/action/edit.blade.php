5fb614a8321f4e652d0c42f0f43d145451902c74
Berke Kadioglu <kadiogluberke@gmail.com>
2024-11-29 04:49:08 +0100

    REFACTOR 🔨  tabbing

--- resources/views/layouts/action/edit.blade.php
+++ resources/views/layouts/action/edit.blade.php
@@ -5,21 +5,21 @@
         <x-error-message/>
 
         <form 
-        action="{{ $action }}"  
-        method="POST" 
-        class="bg-yellow-50 shadow-md rounded px-8 pt-6 pb-8">
+            action="{{ $action }}"  
+            method="POST" 
+            class="bg-yellow-50 shadow-md rounded px-8 pt-6 pb-8">
 
-            @csrf
-            @method('PUT')
-            
-            {{ $slot }}
-            
-            @auth
-                <!-- Update Button -->
-                <div class="flex items-center justify-between mt-3">
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Update</button>
-                </div>
-            @endauth
+                @csrf
+                @method('PUT')
+                
+                {{ $slot }}
+                
+                @auth
+                    <!-- Update Button -->
+                    <div class="flex items-center justify-between mt-3">
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Update</button>
+                    </div>
+                @endauth
         </form>
     </div>
 </body>
