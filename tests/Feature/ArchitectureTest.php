0baed16e1b3bdfc98ee7af353ff31fbf26740e79
Berke Kadioglu <kadiogluberke@gmail.com>
2025-01-29 17:39:54 +0100

    FEATURE ✨ Architecture test

---
+++ tests/Feature/ArchitectureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+it('checks that no dd, dump (ray is allowed) statements are used', function () {
+    expect(['dd', 'dump'])->not()->toBeUsed();
+});
+
+it('checks that interfaces are clean')
+    ->arch('app')->expect("App\Services\Interfaces")
+    ->toBeAbstract()
+    ->toBeInterface();
+    
+it('checks that jobs are ran on the queue only')
+    ->arch('app')->expect("App\Jobs")
+    ->toImplement('Illuminate\Contracts\Queue\ShouldQueue');
